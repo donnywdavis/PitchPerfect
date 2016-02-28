b3dae60c0cfe9afadcf36dfa9bd60abed2c09d66
Donny Davis <donnywdavis@icloud.com>
2016-02-28 09:35:55 -0500

    Add second scene and view controller for that scene

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Donny Davis on 2/28/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playAudioSlow(sender: UIButton) {
+    }
+    
+    @IBAction func playAudioFast(sender: UIButton) {
+    }
+
+    @IBAction func stopPlayingAudio(sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
